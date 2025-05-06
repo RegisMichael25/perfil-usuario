523de863a9f25918ff68cfd5ecf566c5a9a95be8
PedroVictor-PV <pedro.cavalcante0515@gmail.com>
2025-05-06 15:43:20 -0300

    implementacao inicial da classe perfilPessoa

--- src/main/java/br/com/projeto/perfil/model/PerfilPessoa.java
+++ src/main/java/br/com/projeto/perfil/model/PerfilPessoa.java
@@ -1,4 +1,74 @@
 package br.com.projeto.perfil.model;
 
 public class PerfilPessoa {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String description;
+    private String profession;
+    private String academicBackground;
+    private String pathImage;
+
+    public PerfilPessoa(String name, Integer age, String profession){
+        this.name=name;
+        this.age=age;
+        this.profession= profession;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getAcademicBackground() {
+        return academicBackground;
+    }
+
+    public void setAcademicBackground(String academicBackground) {
+        this.academicBackground = academicBackground;
+    }
+
+    public String getPathImage() {
+        return pathImage;
+    }
+
+    public void setPathImage(String pathImage) {
+        this.pathImage = pathImage;
+    }
 }
