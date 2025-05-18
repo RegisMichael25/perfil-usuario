7aa8c150cb4f508e2e6ea35b2e96cb44bb2dcef6
Regis Michael <regis.michael101@gmail.com>
2025-05-18 19:39:09 -0300

    ajuste repository + author

--- src/main/java/br/com/projeto/perfil/repository/PerfilPessoaRepository.java
+++ src/main/java/br/com/projeto/perfil/repository/PerfilPessoaRepository.java
@@ -3,5 +3,10 @@ package br.com.projeto.perfil.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.projeto.perfil.model.PerfilPessoa;
 
+/**
+ * @author Regis Michael
+ * @since 2025-05-18
+ */
+
 public interface PerfilPessoaRepository extends JpaRepository<PerfilPessoa, Long> {
 }
