ac78e86f39f8292cec8635dad250b6bacf2c0f1f
Regis Michael <regis.michael101@gmail.com>
2025-05-18 19:06:08 -0300

    Criando tabela de perfil

--- src/main/resources/db.migration/V0001__create_table-PerfilPessoa.sql
+++ src/main/resources/db.migration/V0001__create_table-PerfilPessoa.sql
@@ -1,18 +1,7 @@
-spring.application.name=api
-
-# Configuracoes do Mysql
-spring.datasource.url=jdbc:mysql://mysql-ong-server-ong-web.d.aivencloud.com:20774/api_dev?ssl-mode=REQUIRED
-spring.datasource.driverClassName=com.mysql.cj.jdbc.Driver
-spring.datasource.username=avnadmin
-spring.datasource.password=AVNS_UgZOpmZlPTgGOHK2Rt5
-
-# JPA/Hibernate
-spring.jpa.database-platform=org.hibernate.dialect.MySQL8Dialect
-spring.jpa.hibernate.ddl-auto=none
-spring.jpa.show-sql=true
-spring.jpa.properties.hibernate.format_sql=true
-
-# Configuracao do Flyway
-spring.flyway.enabled=true
-spring.flyway.locations=classpath:db/migration
-spring.flyway.baseline-on-migrate=true
+CREATE TABLE perfil-pessoa(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    profissao VARCHAR (255) DEFAULT 'desempregado',
+    idade INTEGER NOT NULL,
+    descricao TEXT
+);
