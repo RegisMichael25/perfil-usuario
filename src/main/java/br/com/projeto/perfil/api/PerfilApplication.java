76e26aa05af8aaa96fceac678300b9816bc3679a
RegisMichael25 <regis.michael101@gmail.com>
2025-10-18 09:24:28 -0300

    Alteração de packege

--- src/main/java/br/com/projeto/perfil/api/PerfilApplication.java
+++ src/main/java/br/com/projeto/perfil/api/PerfilApplication.java
@@ -1,4 +1,4 @@
-package br.com.projeto.perfil;
+package br.com.projeto.perfil.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
