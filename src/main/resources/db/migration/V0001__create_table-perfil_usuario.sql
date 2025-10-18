aeb1323cdf0594ae1f45e6421fe7d052e1d12d23
RegisMichael25 <regis.michael101@gmail.com>
2025-10-18 09:25:58 -0300

    Criação e modificação de migration

--- src/main/resources/db/migration/V0001__create_table-perfil_usuario.sql
+++ src/main/resources/db/migration/V0001__create_table-perfil_usuario.sql
@@ -1,10 +1,11 @@
 CREATE TABLE perfil_usuario(
-   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   nome VARCHAR(255) NOT NULL,
-   profissao VARCHAR (155) DEFAULT 'desempregado',
-   idade INTEGER NOT NULL,
-   formacao_academica VARCHAR(150),
-   codigo VARCHAR(255) NOT NULL,
-   caminho_imagem VARCHAR(255),
-   descricao VARCHAR(500)
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    profissao VARCHAR (155) DEFAULT 'desempregado',
+    idade INTEGER NOT NULL,
+    formacao_academica VARCHAR(150),
+    codigo VARCHAR(255) NOT NULL,
+    caminho_imagem VARCHAR(255),
+    descricao VARCHAR(500)
 );
